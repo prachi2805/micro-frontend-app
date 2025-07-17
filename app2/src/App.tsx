0cfdd1ab216e278822a31c66382838ab7f5b6cb4
Prachi Arora <prachiarora0501@gmail.com>
2025-07-17 14:51:05 +0530

    Updated App 1 and App 2 title

--- app2/src/App.tsx
+++ app2/src/App.tsx
@@ -9,7 +9,6 @@ function App() {
   return (
     <>
       <div>
-        App 2
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
